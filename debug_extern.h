5daaa1c0bc17e3b3de3b0a6f2cc96f84ca643e91
rc <rc@localhost.localdomain>
2019-02-20 17:31:27 -0800

    in dev

---
+++ debug_extern.h
@@ -0,0 +1,19 @@
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+#include <stdlib.h>
+#include <ncurses.h>
+#include <string.h>
+#include <menu.h>
+#include <panel.h>
+#include <stdarg.h>
+
+extern WINDOW *Debug_win;
+extern int Debug_line;
+extern int Debug_height;
+extern int Debug_width;
+extern int Debug_line;
+extern int Debug_y_pos;
+extern bool Debug;
+
+#endif
+
